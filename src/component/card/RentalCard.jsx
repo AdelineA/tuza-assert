e79109757297b13b518532df319255a6964925ed
AdelineA <adelinemug6@gmail.com>
2024-05-26 20:54:31 +0200

    feat: add location image on rental card

--- src/component/card/RentalCard.jsx
+++ src/component/card/RentalCard.jsx
@@ -4,10 +4,14 @@ const RentalCard = () => {
   return (      
     <div className="p-6 flex gap-4">
       {rentalData.map((rental, index) => (
-        <div key={index} className="p-4 border-2 border-[#006633] shadow-md rounded-md">
-          <img src={rental.image} alt="house"  className=" h-32"/>
-          <h3 className="text-sm mb-2 italic">{rental.title}</h3>
-          <p>{rental.Description}</p>
+        <div key={index} className="border-2 border-[#006633] shadow-md rounded-md">
+          <img src={rental.image} alt="house"  className="h-44 rounded-lg w-full"/>
+         <div className="flex gap-2 px-3 pt-2 mb-2 ">
+          <img src={rental.img} alt="location" className="h-10 w-10 rounded-full" />
+          <h3 className="text-sm italic pt-2 ">{rental.title}</h3>
+          </div> 
+          <p className="p-4">{rental.Description}</p>
+          <button className="py-3 px-4 font-bold text-base">{rental.viewMore}</button>
         </div>
       ))}
     </div>
--- src/data/rentalData.js
+++ src/data/rentalData.js
@@ -3,20 +3,17 @@ export const rentalData = [
         title:"Easter Province, Bugesera Nyamata",
         Description:" Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor sit amet consectetur adipiscing elit. Sed do eiusmod tempor",
         image:"https://media.istockphoto.com/id/619638736/photo/historic-town-of-rothenburg-ob-der-tauber-franconia-bavaria-germany.jpg?s=1024x1024&w=is&k=20&c=C0Zrhj2emNkTRXam3kVi72fTSsHZ4sBJJ_VCwjEv0KA=",
+        viewMore:"View More",
+        img:"https://img.freepik.com/free-psd/location-icon-composing-maps-regions_314999-2202.jpg?size=626&ext=jpg&ga=GA1.1.48896077.1716732772&semt=sph"        
+
      
     },
     {
         title:"Easter Province, Bugesera Nyamata",
         Description:" Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor sit amet consectetur adipiscing elit. Sed do eiusmod tempor",
-        image:"https://media.istockphoto.com/id/619638736/photo/historic-town-of-rothenburg-ob-der-tauber-franconia-bavaria-germany.jpg?s=1024x1024&w=is&k=20&c=C0Zrhj2emNkTRXam3kVi72fTSsHZ4sBJJ_VCwjEv0KA="
-
-
-        
-    },
-    {
-        title:"Easter Province, Bugesera Nyamata",
-        Description:" Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor sit amet consectetur adipiscing elit. Sed do eiusmod tempor",
-        image:"https://media.istockphoto.com/id/619638736/photo/historic-town-of-rothenburg-ob-der-tauber-franconia-bavaria-germany.jpg?s=1024x1024&w=is&k=20&c=C0Zrhj2emNkTRXam3kVi72fTSsHZ4sBJJ_VCwjEv0KA="
+        image:"https://media.istockphoto.com/id/619638736/photo/historic-town-of-rothenburg-ob-der-tauber-franconia-bavaria-germany.jpg?s=1024x1024&w=is&k=20&c=C0Zrhj2emNkTRXam3kVi72fTSsHZ4sBJJ_VCwjEv0KA=",
+        viewMore:"View More",
+        img:"https://img.freepik.com/free-psd/location-icon-composing-maps-regions_314999-2202.jpg?size=626&ext=jpg&ga=GA1.1.48896077.1716732772&semt=sph"        
 
         
     },
@@ -24,7 +21,16 @@ export const rentalData = [
         title:"Easter Province, Bugesera Nyamata",
         Description:" Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor sit amet consectetur adipiscing elit. Sed do eiusmod tempor",
         image:"https://media.istockphoto.com/id/619638736/photo/historic-town-of-rothenburg-ob-der-tauber-franconia-bavaria-germany.jpg?s=1024x1024&w=is&k=20&c=C0Zrhj2emNkTRXam3kVi72fTSsHZ4sBJJ_VCwjEv0KA=",
-        
+        viewMore:"View More",
+        img:"https://img.freepik.com/free-psd/location-icon-composing-maps-regions_314999-2202.jpg?size=626&ext=jpg&ga=GA1.1.48896077.1716732772&semt=sph"        
+  
+    },
+    {
+        title:"Easter Province, Bugesera Nyamata",
+        Description:" Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed do eiusmod tempor sit amet consectetur adipiscing elit. Sed do eiusmod tempor",
+        image:"https://media.istockphoto.com/id/619638736/photo/historic-town-of-rothenburg-ob-der-tauber-franconia-bavaria-germany.jpg?s=1024x1024&w=is&k=20&c=C0Zrhj2emNkTRXam3kVi72fTSsHZ4sBJJ_VCwjEv0KA=",
+        viewMore:"View More",
+        img:"https://img.freepik.com/free-psd/location-icon-composing-maps-regions_314999-2202.jpg?size=626&ext=jpg&ga=GA1.1.48896077.1716732772&semt=sph"        
     }
  
 ]
